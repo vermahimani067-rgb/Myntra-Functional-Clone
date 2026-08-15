8d6f6aa021636a1b86ff44f4fc350ec551884c46
vermahimani067-rgb <vermahimani067@gmail.com>
2026-08-15 12:33:54 +0000

    update

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+let bagItems = [];
+dsiplayItemsOnHomePage();
+
+function addToBag(itemId) {
+    bagItems.push(itemId);
+}
+
+function dsiplayItemsOnHomePage() {
+    let itemsContainerElement = document.querySelector('.items-container');
+
+let innerHTML = '';
+items.forEach(item => {
+
+    innerHTML += `
+                <div class="item-container">
+                <img class="item-img" src="${item.image}" alt="item image">
+                <div class="rating">
+                    ${item.rating.stars} ⭐️ | ${item.rating.count}
+                </div>
+                <div class="company-name">${item.company}</div>
+                <div class="item-name">${item.item_name}</div>
+                <div class="price">
+                    <span class="current-price">${item.current_price}</span>
+                    <span class="original-price">${item.original_price}</span>
+                    <span class="discount">(${item.discount_percentage}% OFF)</span>
+                </div>
+                <button class="btn-add-bag" onclick="addToBag(${item.id})
+                ">Add to Bag</button>
+                </div>`
+});
+itemsContainerElement.innerHTML = innerHTML;
+}
+
