de3c49ffb6e3909f081cf6c1ac2e80249844e766
vermahimani067-rgb <vermahimani067@gmail.com>
2026-08-16 18:56:07 +0000

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,33 @@
-let bagItems = [];
-dsiplayItemsOnHomePage();
+let bagItems;
+onLoad();
 
+function onLoad() {
+    let bagItemsStr = localStorage.getItem('bagItems');
+    bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+    displayItemsOnHomePage();
+    displayBagIcon();
+}
 function addToBag(itemId) {
     bagItems.push(itemId);
+    localStorage.setItem('bagItems', JSON.stringify(bagItems));
+    displayBagIcon();
 }
 
-function dsiplayItemsOnHomePage() {
+function displayBagIcon() {
+    let bagItemCountElement = document.querySelector('.bag-item-count');
+    if (bagItems.length > 0) {
+        bagItemCountElement.style.visibility = 'visible';
+        bagItemCountElement.innerText = bagItems.length;
+    } else {
+        bagItemCountElement.style.visibility = 'hidden';
+    }
+}
+
+function displayItemsOnHomePage() {
     let itemsContainerElement = document.querySelector('.items-container');
+    if (!itemsContainerElement) {
+        return;
+    }
 
 let innerHTML = '';
 items.forEach(item => {
